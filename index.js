96a0254c0781daacd5db009948c4670c280abc56
Rhea <101016117+StringRays@users.noreply.github.com>
2022-07-15 14:10:42 -0500

    Clean, Organize

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Prints a defult string with inserted function is no text is given to answerText
+//Prints a defult string with inserted function if no text is given to answerText
  function printMadLib() {
      if ( document.getElementById("answerText").value == false ) {
         let strDefault = `This is a noun: ${getNoun()}. This is a verb: ${getVerb()}. This is an adjective: ${getAdjective()}. This is an adverb: ${getAdverb()}.`
@@ -9,54 +9,7 @@
      }
 }
 
-//***********************************************************
-function getVerb() {
-    let verbString = document.getElementById("verbInput").value;
-    if (/[a-z]+/gi.test(verbString) === true) {
-        let verbArray = verbString.match(/[a-z]+/gi);
-        return getRandomFromArray(verbArray);
-    } else {
-        return "(No input given)";
-    }
-}
-
-function getNoun() {
-    let nounString= document.getElementById("nounInput").value;
-    if (/[a-z]+/gi.test(nounString) === true) {
-        let nounArray = nounString.match(/[a-z]+/gi);
-        return getRandomFromArray(nounArray);
-    } else {
-        return "(No input given)";
-    }
-}
-
-function getAdjective() {
-    let adjectiveString= document.getElementById("adjectiveInput").value;
-    if (/[a-z]+/gi.test(adjectiveString) === true) {
-        let adjectiveArray = adjectiveString.match(/[a-z]+/gi);
-        return getRandomFromArray(adjectiveArray);
-    } else {
-        return "(No input given)";
-    }
-}
-
-function getAdverb() {
-    let adverbString= document.getElementById("adverbInput").value;
-    if (/[a-z]+/gi.test(adverbString) === true) {
-        let adverbArray = adverbString.match(/[a-z]+/gi);
-        return getRandomFromArray(adverbArray);
-    } else {
-        return "(No input given)";
-    }
-}
-
-//***************************************************** */
-
-function getRandomFromArray(array) {
-    let index = Math.floor(Math.random()*array.length);
-    return array[index];
-} 
-
+//checks if each character is in the string, and if so replaces it with a word in that category
 function replaceMadLibs(string) {
     if ( /\*/gi.test(string) == true ) {
         string = string.replaceAll(/\*/gi, getNoun);
@@ -73,6 +26,54 @@ function replaceMadLibs(string) {
     return string;
 }
 
+//the following four functions check if words were given in the category, and returns a random one if so and (no input given) if not
+function getVerb() {
+    if (/[a-z]+/gi.test(verbString) === true) {
+        let verbString = document.getElementById("verbInput").value;
+        let verbArray = verbString.match(/[a-z]+/gi);
+        return getRandomFromArray(verbArray);
+    } else {
+        return "(No input given)";
+    }
+}
+
+function getNoun() {
+    if (/[a-z]+/gi.test(nounString) === true) {
+        let nounString= document.getElementById("nounInput").value;
+        let nounArray = nounString.match(/[a-z]+/gi);
+        return getRandomFromArray(nounArray);
+    } else {
+        return "(No input given)";
+    }
+}
+
+function getAdjective() {
+    if (/[a-z]+/gi.test(adjectiveString) === true) {
+        let adjectiveString= document.getElementById("adjectiveInput").value;
+        let adjectiveArray = adjectiveString.match(/[a-z]+/gi);
+        return getRandomFromArray(adjectiveArray);
+    } else {
+        return "(No input given)";
+    }
+}
+
+function getAdverb() {
+    if (/[a-z]+/gi.test(adverbString) === true) {
+        let adverbString= document.getElementById("adverbInput").value;
+        let adverbArray = adverbString.match(/[a-z]+/gi);
+        return getRandomFromArray(adverbArray);
+    } else {
+        return "(No input given)";
+    }
+}
+
+//returns a random word from the passed array
+function getRandomFromArray(array) {
+    let index = Math.floor(Math.random()*array.length);
+    return array[index];
+} 
+
+//clears the answer text box
 function clearText() {
     document.getElementById("answerText").value = null;
 }
